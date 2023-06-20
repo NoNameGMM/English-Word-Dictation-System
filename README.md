0995d5e44425983717ad897d932062aee1622d2a
NoNameGMM <gmm0119@163.com>
2023-06-20 20:43:55 +0800

    v1.1.0 update

--- README.md
+++ README.md
@@ -9,11 +9,23 @@ NoNameGMM制作的英语单词默写系统
 ### 2.添加了十个测试词汇
 ### 3.增加了正确和错误反馈
 
+## v1.1.0
+### 1.增加“清除”和“关于”按钮
+### 2.添加了上海中考考纲单词首字母“a”的单词
+### 3.去除“下一个”按钮
+### 4.修复了正确和错误弹窗后无法聚焦的bug
+
 # Update log
 ## v1.0.0
 ### 1.Add "Submit" and "Next" buttons
 ### 2.Added ten test vocabulary
 ### 3.Added correct and incorrect feedback
+
+## v1.1.0
+### 1.Add "Clear" and "About" buttons
+### 2.Added the word with the initial letter "a" in the Shanghai High School Entrance Examination syllabus
+### 3.Remove the 'Next' button
+### 4.Fixed bug where correct and incorrect pop ups prevented focusing
 
 ## 暂时只有中文版本
 ## Currently only available in Chinese
--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function readTextFile(file, callback) {
 function loadword (){
     readTextFile("word/examination-outline.json", function(text)
     {
-        var random = Math.round(Math.random()*9);
+        var random = Math.round(Math.random()*114);
         var data = JSON.parse(text);
         var translate = new Array(1000);
         var word = new Array(1000);
-        for(var i = 0;i <= 9;i++)
+        for(var i = 0;i <= 114;i++)
         {
             word[i] = data.word[i];
             translate[i] = data.translate[i];
@@ -24,6 +24,88 @@ function loadword (){
         document.getElementById("demo").innerHTML = translate[random];
         document.getElementById("answertrue").value = word[random];
     });
+}
+
+//type:success, error, info
+//time:ms
+function displayAlert(type, data, time)
+{
+    var lunbo=document.createElement("div");
+    if(type == "success") 
+    {
+        lunbo.style.backgroundColor = "#009900";
+        lunbo.id="lunbo";
+        lunbo.style.position = "absolute";
+        lunbo.style.width = "200px";
+        lunbo.style.height = "60px";
+        lunbo.style.top = "50%";
+        lunbo.style.left = "50%";
+        lunbo.style.right = "0";
+        lunbo.style.bottom = "0";
+        lunbo.style.transform = "translate(-50%, -50%)";
+        lunbo.style.color = "white";
+        lunbo.style.fontSize = "25px";
+        lunbo.style.borderRadius = "20px";
+        lunbo.style.textAlign="center";
+        lunbo.style.lineHeight="60px";
+    } 
+    else if(type == "error") 
+    {
+        lunbo.style.backgroundColor = "#990000";
+        lunbo.id="lunbo";
+        lunbo.style.position = "absolute";
+        lunbo.style.width = "200px";
+        lunbo.style.height = "60px";
+        lunbo.style.top = "50%";
+        lunbo.style.left = "50%";
+        lunbo.style.right = "0";
+        lunbo.style.bottom = "0";
+        lunbo.style.transform = "translate(-50%, -50%)";
+        lunbo.style.color = "white";
+        lunbo.style.fontSize = "25px";
+        lunbo.style.borderRadius = "20px";
+        lunbo.style.textAlign="center";
+        lunbo.style.lineHeight="60px";
+    }
+    else if(type == "info")
+    {
+        lunbo.style.backgroundColor = "#e6b800";
+        lunbo.id="lunbo";
+        lunbo.style.position = "absolute";
+        lunbo.style.width = "200px";
+        lunbo.style.height = "120px";
+        lunbo.style.top = "50%";
+        lunbo.style.left = "50%";
+        lunbo.style.right = "0";
+        lunbo.style.bottom = "0";
+        lunbo.style.transform = "translate(-50%, -50%)";
+        lunbo.style.color = "white";
+        lunbo.style.fontSize = "20px";
+        lunbo.style.borderRadius = "20px";
+        lunbo.style.textAlign="center";
+        lunbo.style.lineHeight="60px";
+    }
+
+    if(document.getElementById("lunbo")==null)
+    {
+        document.body.appendChild(lunbo);
+        lunbo.innerHTML=data;
+        setTimeout(function(){document.body.removeChild(lunbo);} ,time);
+    }
+}
+
+function truely() 
+{
+    displayAlert("success", "答案正确", 1500);
+}
+
+function wrongly() 
+{
+    displayAlert("error", "答案错误", 1500);
+}
+function information() 
+{
+    displayAlert("info", "作者 : NoNameGMM 版本 : V1.1.0", 1500);
 }
 
 loadword();
